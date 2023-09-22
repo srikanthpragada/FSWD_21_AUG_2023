adf5adba7d1439c1f9b7b030047713843a30d5f5
srikanthpragada <srikanthpragada@gmail.com>
2023-09-22 20:56:17 +0530

    22-SEP

--- express/dbaccess/server.js
+++ express/dbaccess/server.js
@@ -17,15 +17,29 @@ app.get('/', (req, res) => {
     res.end("<h1>DB Access Demo </h1>")
 })
 
-app.get('/dept/add', (req, res) => {
-    res.render("deptadd")
+async function getDepts() {
+    try {
+        const con = await mysql.createConnection(conDetails)
+        const [rows] = await con.execute("select * from departments")
+        con.end()
+        return rows
+    }
+    catch (ex) {
+        throw ex;
+    }
+}
+
+app.get('/dept/list', async (req, res) => {
+    try {
+       const rows = await getDepts()
+       res.render('deptlist', {depts : rows})
+    }
+    catch(ex) {
+        console.log(ex)
+        res.send("<h2>Sorry! Could not get details!</h2>")
+    }
 })
 
-app.get('/dept/delete', (req, res) => {
-    const deptid = req.query.deptid 
-    const result = deleteDept(deptid)
-    res.end("<h1>Deleted!!</h1>")
-})
 
 async function deleteDept(id) {
     try {
@@ -34,15 +48,28 @@ async function deleteDept(id) {
                           [id])
         con.end()
         if(result.affectedRows === 1)                          
-            return true 
+            return 1
         else
-            return false
+            return 0
     }
     catch (ex) {
         console.log(ex)
-        return false
+        return -1
     }
 }
+
+app.get('/dept/delete/:deptid', async (req, res) => {
+    const deptid = req.params.deptid 
+    const result = await deleteDept(deptid)
+    if(result === 1) // success 
+      res.end("<h1>Deleted!!</h1>")
+    else
+     if(result === 0) // not found 
+         res.send("<h1>Dept Id Not Found </h1>")
+     else // error
+         res.send("<h1>Could not delete due to error! </h1>")
+})
+
 
 async function addDept(id, name) {
     try {
@@ -61,15 +88,67 @@ async function addDept(id, name) {
     }
 }
 
-app.post('/dept/add', (req, res) => {
+app.get('/dept/add', (req, res) => {
+    res.render("deptadd")
+})
+
+app.post('/dept/add', async (req, res) => {
     // process form data
     const { deptid, deptname } = req.body
-    let result = addDept(deptid, deptname)
-    console.log(result)
+    let result = await addDept(deptid, deptname)
     if (result)
-        res.end("<h1>Department has been added successfully!!")
+        res.redirect("/dept/list")
     else
-        res.end("<h1>Department could not be added!!")
+        res.send("<h1>Department could not be added!!")
+})
+
+
+async function getDept(id) {
+    try {
+        const con = await mysql.createConnection(conDetails)
+        const [rows] = await con.execute("select * from departments where dept_id = ?", [id])
+        //console.log(rows)
+        con.end()
+        return rows[0]
+    }
+    catch (ex) {
+        throw ex;
+    }
+}
+
+
+async function updateDept(deptid, deptname) {
+    try {
+        const con = await mysql.createConnection(conDetails)
+        const [result] = await con.execute
+                ("update departments set dept_name = ? where dept_id = ?",
+                          [deptname, deptid])
+        con.end()
+        if(result.affectedRows === 1)                          
+            return true 
+        else
+            return false 
+    }
+    catch (ex) {
+        console.log(ex)
+        return false
+    }
+}
+
+app.get('/dept/edit/:id', async (req, res) => {
+    const deptid = req.params.id
+    const dept = await getDept(deptid)
+    res.render("deptedit", {dept : dept})
+})
+
+app.post('/dept/edit/:id', async (req, res) => {
+    const deptid = req.params.id
+    const deptname = req.body.deptname 
+    const result = await updateDept(deptid, deptname)
+    if (result)
+       res.redirect("/dept/list")
+    else
+       res.send("<h2>Sorry! Could not update department </h2>")
 })
 
 
